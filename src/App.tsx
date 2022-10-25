e6fc9b646e0955c40ddcbfa13f4e187f1111c64b
Isaac Kabucho <kabszac@gmail.com>
2022-10-25 13:58:58 +0300

    Added changes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './App.css';
+import MuiRating from './components/MuiRating';
+//import MuiCheckbox from './components/MuiCheckbox';
+//import MuiSwitch from './components/MuiSwitch';
+//import {MuiRadioButton} from './components/MUIRadioButton';
+//import MUISelect from './components/MUISelect';
 //import MuiButton from './components/MuiButton';
-import MUITextField from './components/MUITextField';
+//import MUITextField from './components/MUITextField';
 // import MuiTypography from './components/MuiTypography';
 
 function App() {
@@ -9,7 +14,12 @@ function App() {
     <div className="App">
       {/* <MuiTypography/> */}
       {/* <MuiButton/> */}
-      <MUITextField/>
+      {/* <MUITextField/> */}
+      {/* <MUISelect/> */}
+      {/* <MuiRadioButton/> */}
+      {/* <MuiCheckbox/> */}
+      {/* <MuiSwitch/> */}
+      <MuiRating/>
     </div>
   );
 }
