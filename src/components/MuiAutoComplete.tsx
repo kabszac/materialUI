8e73eae806b1d075eb361bd8a0f4bfa7c2c6c4e3
Isaac Kabucho <kabszac@gmail.com>
2022-11-03 11:14:27 +0300

    Added autocomplete

---
+++ src/components/MuiAutoComplete.tsx
@@ -0,0 +1,37 @@
+import { Autocomplete, Stack, TextField } from '@mui/material';
+import React, {useState} from 'react';
+
+type Skill = {
+    id: number,
+    label: string
+}
+
+const skills = ['HTML', 'CSS', 'JS', 'React']
+
+const skillOptions = skills.map((skill, index) => ({id: index+1, label: skill}))
+
+const MuiAutoComplete = () => {
+    const [value, setValue] = useState<string | null>(null)
+    const [skill, setSkill] = useState<Skill | null>(null)
+
+    return (
+        <Stack spacing={2} width='250px'>
+            <Autocomplete 
+                options={skills} 
+                renderInput={(params) => <TextField {...params} label='skills'/>} 
+                value={value}
+                onChange={(event:any, newValue: string | null) => setValue(newValue) }
+                freeSolo
+            />
+
+            <Autocomplete 
+                options={skillOptions} 
+                renderInput={(params) => <TextField {...params} label='skills'/>} 
+                value={skill}
+                onChange={(event:any, newValue: Skill | null) => setSkill(newValue) }
+            />
+        </Stack>
+    );
+};
+
+export default MuiAutoComplete;
